3edef09229642f682fc979ef8a4f3c9950035b06
debian-mocos <130593875+MoXcz@users.noreply.github.com>
2024-11-02 02:38:25 -0400

    Fix playRound() logic and add scores
    The function playRound() has two parameters, but they weren't used
    because those same arguments were assigned inside the function. To
    remain constant with the function declaration, the parameter assignment
    is now outside the function and declared as constants.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let humanScore = 0;
+let computerScore = 0;
+const humanChoice = getHumanChoice().toLowerCase();
+const computerChoice = getComputerChoice().toLowerCase();
+playRound(humanChoice, computerChoice);
+
 function getComputerChoice() {
   let choice = parseInt(Math.random() * 3);
   if (choice == 0) {
@@ -14,36 +20,40 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-  humanChoice = getHumanChoice().toLowerCase();
-  computerChoice = getComputerChoice().toLowerCase();
   if (humanChoice == "rock") {
     if (computerChoice == "rock") {
       console.log("Tie!");
     }
     if (computerChoice == "paper") {
       console.log("You Lose! Paper beats Rock");
+      computerScore++;
     }
     if (computerChoice == "scissors") {
       console.log("You Win! Rock beats Scissors");
+      humanScore++;
     }
   }
   if (humanChoice == "paper") {
     if (computerChoice == "rock") {
       console.log("You Win! Paper beats Rock");
+      humanScore++;
     }
     if (computerChoice == "paper") {
       console.log("Tie!");
     }
     if (computerChoice == "scissors") {
       console.log("You Lose! Scissors beat Paper");
+      computerScore++;
     }
   }
   if (humanChoice == "scissors") {
     if (computerChoice == "rock") {
       console.log("You Lose! Rock beats Scissors");
+      computerScore++;
     }
     if (computerChoice == "paper") {
       console.log("You Win! Scissors beat Paper");
+      humanScore++;
     }
     if (computerChoice == "scissors") {
       console.log("Tie!");
