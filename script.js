9880cb0dae4157035470d4ef740b0e31ead44261
debian-mocos <130593875+MoXcz@users.noreply.github.com>
2024-11-02 02:32:26 -0400

    Add function to play a round with a prompt
    The game only plays one time when playRound() is called.

--- script.js
+++ script.js
@@ -8,3 +8,45 @@ function getComputerChoice() {
     return "scissors";
   }
 }
+
+function getHumanChoice() {
+  return prompt("Enter one of the following: 'rock', 'paper, 'scissors'");
+}
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = getHumanChoice().toLowerCase();
+  computerChoice = getComputerChoice().toLowerCase();
+  if (humanChoice == "rock") {
+    if (computerChoice == "rock") {
+      console.log("Tie!");
+    }
+    if (computerChoice == "paper") {
+      console.log("You Lose! Paper beats Rock");
+    }
+    if (computerChoice == "scissors") {
+      console.log("You Win! Rock beats Scissors");
+    }
+  }
+  if (humanChoice == "paper") {
+    if (computerChoice == "rock") {
+      console.log("You Win! Paper beats Rock");
+    }
+    if (computerChoice == "paper") {
+      console.log("Tie!");
+    }
+    if (computerChoice == "scissors") {
+      console.log("You Lose! Scissors beat Paper");
+    }
+  }
+  if (humanChoice == "scissors") {
+    if (computerChoice == "rock") {
+      console.log("You Lose! Rock beats Scissors");
+    }
+    if (computerChoice == "paper") {
+      console.log("You Win! Scissors beat Paper");
+    }
+    if (computerChoice == "scissors") {
+      console.log("Tie!");
+    }
+  }
+}
